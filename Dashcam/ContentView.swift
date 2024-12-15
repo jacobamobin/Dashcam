32703151a623886a4ec8e80cdf275bd344385615
Jacob Mobin <jacobamobin@gmail.com>
2024-12-15 13:59:51 -0500

    Initial Commit

---
+++ Dashcam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dashcam
+//
+//  Created by Jacob Mobin on 2024-12-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dashcam/DashcamApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashcamApp.swift
+//  Dashcam
+//
+//  Created by Jacob Mobin on 2024-12-15.
+//
+
+import SwiftUI
+
+@main
+struct DashcamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
