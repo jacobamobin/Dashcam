e05ced597443b07d98a4a2e56648f87c3a3ffa88
Jacob Mobin <jacobamobin@gmail.com>
2024-12-15 14:08:16 -0500

    Update

---
+++ Dashcam/News/Article.swift
@@ -0,0 +1,18 @@
+//
+//  Article.swift
+//  Dashcam
+//
+//  Created by Jacob Mobin on 2024-12-15.
+//
+
+import SwiftUI
+
+struct Article: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Article()
+}
